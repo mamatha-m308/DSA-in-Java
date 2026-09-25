314738f065a21ae8b91030c5e89d1daa9803fbed
mamatha m <167190861+mamatha-m308@users.noreply.github.com>
2026-09-25 21:20:37 +0530

    Fix formatting

--- Arrays/Majority_element.java
+++ Arrays/Majority_element.java
@@ -3,7 +3,7 @@
 class Solution {
     public int majorityElement(int[] nums) {
         int count = 0;   
-        int current = 0;
+         int current = 0;
 
         for (int i = 0; i < nums.length; i++) {
 
